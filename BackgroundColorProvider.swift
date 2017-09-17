613a367f816a6f403fc4681bdefa246cba85e34a
paulmorello <paulmorello1@gmail.com>
2017-09-17 12:25:26 -0700

    Added additional colours to the random colour generator

--- BackgroundColorProvider.swift
+++ BackgroundColorProvider.swift
@@ -16,7 +16,13 @@ struct BackgroundColorProvider {
         UIColor(red: 124/255.0, green: 86/255.0, blue: 230/255.0, alpha: 1.0),
         UIColor(red: 23/255.0, green: 200/255.0, blue: 58/255.0, alpha: 1.0),
         UIColor(red: 140/255.0, green: 92/255.0, blue: 130/255.0, alpha: 1.0),
-        UIColor(red: 168/255.0, green: 76/255.0, blue: 76/255.0, alpha: 1.0)
+        UIColor(red: 168/255.0, green: 76/255.0, blue: 76/255.0, alpha: 1.0),
+        UIColor(red: 200/255.0, green: 123/255.0, blue: 46/255.0, alpha: 1.0),
+        UIColor(red: 100/255.0, green: 12/255.0, blue: 60/255.0, alpha: 1.0),
+        UIColor(red: 65/255.0, green: 23/255.0, blue: 123/255.0, alpha: 1.0),
+        UIColor(red: 89/255.0, green: 13/255.0, blue: 110/255.0, alpha: 1.0),
+        UIColor(red: 220/255.0, green: 160/255.0, blue: 240/255.0, alpha: 1.0),
+        UIColor(red: 182/255.0, green: 200/255.0, blue: 90/255.0, alpha: 1.0)
     ]
     
     func randomColor() -> UIColor {
